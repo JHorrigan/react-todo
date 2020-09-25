db65dc4be16fe4f1c456b89147b54feebd3cb870
JHorrigan <jahorrigan1411@gmail.com>
2020-09-25 16:43:17 +0100

    Toggle Completed Status in State

--- src/App.js
+++ src/App.js
@@ -25,7 +25,14 @@ class App extends Component {
   }
 
   markComplete = (id) => {
-    console.log(id);
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
   }
 
   render() {
