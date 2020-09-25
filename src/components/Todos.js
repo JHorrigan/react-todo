b240b9f086b9c0ecb29be27fb3f2116ed355c50e
JHorrigan <jahorrigan1411@gmail.com>
2020-09-25 16:05:48 +0100

    Component Drilling - Pass Method from App.js So State is in Scope

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,13 +3,10 @@ import TodoItem from './Todoitem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component {
-  markComplete = () => {
-      console.log('Hello');
-  }
   render () {
     return this.props.todos.map((todo) =>(
         <TodoItem key={todo.id} todo={todo} markComplete=
-        {this.markComplete} />
+        {this.props.markComplete} />
     ));
   }
 }
