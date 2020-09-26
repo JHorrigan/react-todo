855d615f103b7c336a71915f0ae11e54c7511b7c
JHorrigan <jahorrigan1411@gmail.com>
2020-09-26 09:06:36 +0100

    addTodo Method in App Passed Down to AddTodo

--- src/App.js
+++ src/App.js
@@ -45,12 +45,24 @@ class App extends Component {
     });
   }
 
+  // Add Task
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo}/>
           <Todos todos={this.state.todos} markComplete={this.markComplete}
           deleteTask={this.deleteTask} />
         </div>
